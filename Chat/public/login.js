ee5faa36ffcb5d028ecdd542cb4ebb014d324fed
Lening Steve <zidane-steve.lening-talaupa@grenoble-inp.org>
2024-12-12 10:58:45 +0100

    ajout de connexion admin

--- Chat/public/login.js
+++ Chat/public/login.js
@@ -1,23 +1,38 @@
 function loadDOM() {
     const submitButton = document.getElementById("submit");
+    const retourButton = document.getElementById("retourLogin");
 
     // Handle sumit login form
     submitButton.onclick = () => {
-        const login = document.getElementById("login");
-        const password = document.getElementById("password");
-        if(login.value === "login" && password.value === "password"){
-            // on conserve la session de l'admin
-            localStorage.setItem("adminUser", true);
-            // on redirige vers la page admin
-            window.location.href="admin.html";
-            // admin doit contenir un bouton retour pour revenir a la 
-            // page du chat et un bouton logout qui va faire passer 
-            // adminUser a false.
-        }
-        else{
-            alert("Identifiants incorrects");
-        }
+        const login = document.getElementById("login").value;
+        const password = document.getElementById("password").value;
+        
+        fetch('/admin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ login, password }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // on conserve la session de l'admin
+                localStorage.setItem("adminUser", "true");
+                // on redirige vers la page admin
+                window.location.href="admin.html";
+            } else {
+                alert("Identifiants incorrects");
+            }
+        })
+        .catch(error => {
+            console.error("Erreur lors de la connexion");
+        });
     };
+
+    retourButton.onclick = () => {
+        window.location.href="index.html";
+    }
 }
 // Fin du chargement du DOM
 document.addEventListener('DOMContentLoaded', loadDOM);
